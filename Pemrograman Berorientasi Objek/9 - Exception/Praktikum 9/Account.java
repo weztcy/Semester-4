aea808b3a7e183a01f08711d3cdd6954d04afa70
Dimas Pratama <yohanesdimas888@gmail.com>
2023-06-25 14:33:37 +0700

    Update

---
+++ Pemrograman Berorientasi Objek/9 - Exception/Praktikum 9/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    protected double balance;
+
+    protected Account(double initBalance) {
+        balance = initBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amt) {
+        balance = balance + amt;
+    }
+
+    public void withdraw(double amt) throws OverdraftException {
+        if (amt <= balance) {
+            balance = balance - amt;
+        }
+        else {
+            throw new OverdraftException("Dana Anda tidak cukup", amt-balance);
+        }
+    }
+}
